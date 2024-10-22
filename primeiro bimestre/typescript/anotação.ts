9bc122edaf28187c6e17dd7c1a246af7423739d5
GASBIX <160485857+GASBIX@users.noreply.github.com>
2024-10-22 17:55:27 -0300

    porra 2

--- primeiro bimestre/typescript/anotação.ts
+++ primeiro bimestre/typescript/anotação.ts
@@ -8,4 +8,11 @@ console.log(instagram);
 
 //primeira anotção de typescript
 
+//interface de pessoa com typescript
+interface pessoa {
+    nome: string;
+}
+ console.log(`olá, meu nome é ${nome} e ja posso virar clt `)
+
+
 
